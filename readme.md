cd86e8f1a264799c494df71664602e5b2dc9c6a5
Anton Novojilov <andy@essentialkaos.com>
2017-05-02 07:56:25 -0400

    Prerelease

--- readme.md
+++ readme.md
@@ -1,1 +1,34 @@
-# translit
+## `translit` [![GoDoc](https://godoc.org/pkg.re/essentialkaos/translit.v1?status.svg)](https://godoc.org/pkg.re/essentialkaos/translit.v1) [![Go Report Card](https://goreportcard.com/badge/github.com/essentialkaos/translit)](https://goreportcard.com/report/github.com/essentialkaos/translit) [![codebeat badge](https://codebeat.co/badges/ffffff)](https://codebeat.co/projects/github-com-essentialkaos-translit) [![Coverage Status](https://coveralls.io/repos/github/essentialkaos/translit/badge.svg?branch=develop)](https://coveralls.io/github/essentialkaos/translit?branch=develop) [![License](https://gh.kaos.io/ekol.svg)](https://essentialkaos.com/ekol)
+
+`translit` is a Go package for Russian text transliteration.
+
+### Installation
+
+Before the initial install allows git to use redirects for [pkg.re](https://github.com/essentialkaos/pkgre) service (reason why you should do this described [here](https://github.com/essentialkaos/pkgre#git-support)):
+
+```
+git config --global http.https://pkg.re.followRedirects true
+```
+
+Make sure you have a working Go 1.5+ workspace ([instructions](https://golang.org/doc/install)), then:
+
+```
+go get pkg.re/essentialkaos/translit.v1
+```
+
+For update to latest stable release, do:
+
+```
+go get -u pkg.re/essentialkaos/translit.v1
+```
+
+### Build Status
+
+| Branch | Status |
+|------------|--------|
+| `master` | [![Build Status](https://travis-ci.org/essentialkaos/translit.svg?branch=master)](https://travis-ci.org/essentialkaos/translit) |
+| `develop` | [![Build Status](https://travis-ci.org/essentialkaos/translit.svg?branch=develop)](https://travis-ci.org/essentialkaos/translit) |
+
+### License
+
+[EKOL](https://essentialkaos.com/ekol)
---
+++ translit.go
@@ -0,0 +1,111 @@
+// Package translit convert russian text to translit
+
+package translit
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2017 ESSENTIAL KAOS                         //
+//        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"bytes"
+	"strings"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+var universal = map[rune]string{
+	'а': "a", 'б': "b", 'в': "v", 'г': "g", 'д': "d", 'е': "e", 'ё': "jo", 'ж': "zh",
+	'з': "z", 'и': "i", 'й': "j", 'к': "k", 'л': "l", 'м': "m", 'н': "n", 'о': "o",
+	'п': "p", 'р': "r", 'с': "s", 'т': "t", 'у': "u", 'ф': "f", 'х': "h", 'ц': "c",
+	'ч': "ch", 'ш': "sh", 'щ': "shh", 'ъ': "\"", 'ы': "y", 'ь': "'", 'э': "je",
+	'ю': "ju", 'я': "ja",
+}
+
+var gost779b = map[rune]string{
+	'а': "a", 'б': "b", 'в': "v", 'г': "g", 'д': "d", 'е': "e", 'ё': "yo", 'ж': "zh",
+	'з': "z", 'и': "i", 'й': "j", 'к': "k", 'л': "l", 'м': "m", 'н': "n", 'о': "o",
+	'п': "p", 'р': "r", 'с': "s", 'т': "t", 'у': "u", 'ф': "f", 'х': "x", 'ц': "c",
+	'ч': "ch", 'ш': "sh", 'щ': "shh", 'ъ': "\"", 'ы': "y", 'ь': "'", 'э': "eh",
+	'ю': "yu", 'я': "ya",
+}
+
+var iso9 = map[rune]string{
+	'а': "a", 'б': "b", 'в': "v", 'г': "g", 'д': "d", 'е': "e", 'ё': "ë", 'ж': "ž",
+	'з': "z", 'и': "i", 'й': "j", 'к': "k", 'л': "l", 'м': "m", 'н': "n", 'о': "o",
+	'п': "p", 'р': "r", 'с': "s", 'т': "t", 'у': "u", 'ф': "f", 'х': "h", 'ц': "c",
+	'ч': "č", 'ш': "š", 'щ': "ŝ", 'ъ': "\"", 'ы': "y", 'ь': "'", 'э': "è",
+	'ю': "û", 'я': "â",
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Universal convert text with default mappings
+func Universal(text string) string {
+	return convert(text, universal)
+}
+
+// GOST779B convert text with GOST 7.79B mappings
+func GOST779B(text string) string {
+	return convert(text, gost779b)
+}
+
+// ISO9 convert text with ISO 9 mappings
+func ISO9(text string) string {
+	return convert(text, iso9)
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func convert(text string, mapping map[rune]string) string {
+	if text == "" {
+		return ""
+	}
+
+	var buffer = bytes.NewBuffer(nil)
+
+	for _, r := range text {
+		if !isRussianChar(r) {
+			buffer.WriteRune(r)
+			continue
+		}
+
+		if isRussianCapsChar(r) {
+			buffer.WriteString(strings.Title(mapping[getLowerRune(r)]))
+			continue
+		}
+
+		buffer.WriteString(mapping[r])
+	}
+
+	return buffer.String()
+}
+
+func isRussianChar(r rune) bool {
+	switch {
+	case r >= 1040 && r <= 1103,
+		r == 1105, r == 1025:
+		return true
+	}
+
+	return false
+}
+
+func isRussianCapsChar(r rune) bool {
+	switch {
+	case r >= 1040 && r <= 1071, r == 1025:
+		return true
+	}
+
+	return false
+}
+
+func getLowerRune(r rune) rune {
+	if r == 1025 {
+		return 1105
+	}
+
+	return r + 32
+}
